0f1c3d3769115beac0339953270491f3f2851b2a
Endermandels <elijahdelavar@gmail.com>
2025-12-22 15:10:02 -0800

    Add cameras to each client

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,13 @@ func _enter_tree() -> void:
 	# Make sure client has authority over controlling the player
 	set_multiplayer_authority(name.to_int())
 
+func _ready() -> void:
+	if not is_multiplayer_authority(): return
+
+	# Each client should have its own camera
+	var camera = Camera2D.new()
+	add_child(camera)
+
 func _physics_process(_delta: float) -> void:
 	# Make sure only client has authority to move player
 	if not is_multiplayer_authority(): return
